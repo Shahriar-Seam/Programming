7e775573ca3f758c4ea2c5fb46b16b304abf0eb3
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2023-09-16 15:55:48 +0600

    Update b_1010.c

--- BeeCrowd/b_1010.c
+++ BeeCrowd/b_1010.c
@@ -8,7 +8,7 @@ int main()
     scanf("%d %lf %lf", &code1, &a, &A);
     scanf("%d %lf %lf", &code2, &b, &B);
 
-    printf("VALOR A PAGAR: R$ %0.2lf\n", (a * A) +(b * B));
+    printf("VALOR A PAGAR: R$ %0.2lf\n", (a * A) + (b * B));
 
     return 0;
 }
