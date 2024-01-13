3ea572d23c2e8cbd5f2e077e2ea28edc26f168f0
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-01-13 23:13:12 +0600

    .

--- temp.py
+++ temp.py
@@ -1,29 +1,3 @@
-import pyttsx3 as kazi
-import random as r
-import time
+a, b, c = map(int, input().split())
 
-def run():
-    exit()
-    
-def kobul():
-    kazi.speak("kobul")
-    
-for cnt in range (0, 3):
-    kobul()
-    
-rifat = True
-
-r.seed()
-
-acr = r.randint(0, 1)
-
-rifat = True if acr else False
-
-time.sleep(1)
-
-if rifat:
-    kazi.speak("Yes")
-else:
-    kazi.speak("No")
-
-run()
+print(a + b + c)
