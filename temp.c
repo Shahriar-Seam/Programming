55186c82252911b2b295cd354f288ca35d0fc406
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-01-30 21:03:51 +0600

    .

--- temp.c
+++ temp.c
@@ -1,13 +1,41 @@
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-int bla(int a, int b)
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
 {
-    return b;
+  g_print ("Hello World\n");
 }
 
-int main()
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
 {
-    printf("%d %d\n", bla(2, 4), bla(2, 3));
+  GtkWidget *window;
+  GtkWidget *button;
 
-    return 0;
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Hello");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  button = gtk_button_new_with_label ("Hello World");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  gtk_window_set_child (GTK_WINDOW (window), button);
+
+  gtk_window_present (GTK_WINDOW (window));
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
