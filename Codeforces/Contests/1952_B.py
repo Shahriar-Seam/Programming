5e0367a8b3edbbaf3a6ef661bbeb5bf79113f591
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-04-01 21:59:48 +0600

    .

---
+++ Codeforces/Contests/1952_B.py
@@ -0,0 +1,10 @@
+import math
+
+for i in range(1, int(input()) + 1):
+    s = input()
+    
+    if "it" in s:
+        print("YES")
+    else:
+        print("NO")
+    
---
+++ Codeforces/Contests/1952_J.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+one = "yoink a\nyoink b\n*slaps a on top of b*\nyeet b\ngo touch some grass"
+two = "yoink a\nbruh b is lowkey just 0\nrip this b fell off by a\nvibe check a ratios b\nsimp for 7\nbruh a is lowkey just b\nyeet a\ngo touch some grass"
+three = "yoink n\nyoink a\nbruh m is lowkey just a[0]\nbruh i is lowkey just 1\nvibe check n ratios i\nsimp for 9\nyeet m\ngo touch some grass\nvibe check a[i] ratios m\nbruh m is lowkey just a[i]\n*slaps 1 on top of i*\nsimp for 5"
+four = """
+    yoink n
+    yoink a
+"""
+
+if n == 1:
+    print(one)
+elif n == 2:
+    print(two)
+elif n == 3:
+    print(three)
+else:
+    print(four)
