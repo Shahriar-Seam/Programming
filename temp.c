0af34d82b1aca783bd904ae2394f4ea4d7344e40
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-04-03 20:52:03 +0600

    .

--- temp.c
+++ temp.c
@@ -1,17 +1,54 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-// #define M_PI 3.1415926535897932384626433832795028841971693993751L
+typedef struct node Node;
 
-int main(void) {
+struct node {
+    int data;
+    Node *next;
+};
 
-    const long double PI = M_PI;
+Node *create_node(int data, Node *next)
+{
+    Node *new_node = (Node *) malloc(sizeof(Node));
 
-    printf("Size of double: %d\n", (int)sizeof(double));
-    printf("Size of long double: %d\n", (int)sizeof(long double));
+    new_node->data = data;
+    new_node->next = next;
 
-    printf("PI == %.6Lg\n", PI);
+    return new_node;
+}
+
+int main()
+{
+    Node *head, *current_node, *new_node;
+    int data, i, n;
+
+    printf("Enter number of nodes\n");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &data);
+
+        new_node = create_node(data, NULL);
+
+        if (i == 0) {
+            head = new_node;
+            current_node = new_node;
+        }
+        else {
+            current_node->next = new_node;
+
+            current_node = current_node->next;
+        }
+    }
+
+    current_node = head;
+
+    while (current_node != NULL) {
+        printf("%d\n", current_node->data);
+
+        current_node = current_node->next;
+    }
 
     return 0;
-
 }
