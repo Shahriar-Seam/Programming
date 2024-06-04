e8de8268d2798fd19ac507aa318922f9c5128714
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-06-04 19:11:51 +0600

    .

---
+++ USACO/Bronze/intro-ds/test.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+
+    getline(cin >> ws, s);
+
+    cout << s;
+
+    return 0;
+}
