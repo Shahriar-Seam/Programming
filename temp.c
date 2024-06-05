2536678eaa41474ed45c16877f354f33eb5f53fd
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-06-05 08:26:06 +0600

    .

--- temp.c
+++ temp.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
+
 int main()
 {
     int nums[] = {1, 3, 1, 7};
-    int q = [1, 3, 2, 4];
     int x = 1;
+
+    printf("%d\n", gcd(-4, 2));
 
     return 0;
 }
