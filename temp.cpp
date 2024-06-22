9b2245b4640a08b00745da41eddf344eb245ceca
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-06-22 15:52:10 +0600

    .

--- temp.cpp
+++ temp.cpp
@@ -7,19 +7,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int arr[] = {5, 6, 7, 8, 9};
-    int i, j;
-    set <int> s;
-
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 5; j++) {
-            s.insert((arr[i] + arr[j]) % 10);
-        }
-    }
-
-    for (auto it : s) {
-        cout << it << " ";
-    }
+    
 
     return 0;
 }
--- temp.py
+++ temp.py
@@ -1,18 +1,1 @@
-def encode(strs: list[str]) -> str:
-    string = ""
-
-    for s in strs:
-        string = string + "#" + s
-
-    return string
-
-def decode(s: str) -> list[str]:
-    strs = s.split('#')
-    strs.pop(0)
-
-    return strs
-
-strs = ["neet", "code", "love", "you"]
-
-print(encode(strs))
-print(decode(encode(strs)))
+print(eval(input()))
