45b94f6e001510f9bd9c69df6e038c93c82096e4
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-09-04 13:29:58 +0600

    .

--- temp.c
+++ temp.c
@@ -1,25 +1,66 @@
 #include <stdio.h>
-int main()
+#include <string.h>
+#include <stdlib.h>
+
+void clean_dump(char *dump, char *filename)
 {
-    double gpa[8], credit[8], product, sum = 0, sum1 = 0;
-    int i, j, k;
-    for (i = 0; i < 8; i++)
-    {
-        //    taking gpa as input
-        scanf("%lf", &gpa[i]);
-        // taking credit as input
-        scanf("%lf", &credit[i]);
+    char temp_dump[500], temp_file[500];
+    int i, j, len = strlen(filename);
+
+    for (i = 0, j = 0; dump[i] != '\0'; i++) {
+        if (dump[i] == '\"') {
+            i++;
+
+            while (dump[i] != '\0' && dump[i] != '\"') {
+                temp_dump[j++] = dump[i++];
+            }
+
+            break;
+        }
     }
 
-    // finding the multiplication of gpa and credit hours
-    for (i = 0; i < 8; i++)
-    {
-        product = gpa[i] * credit[i];
-        sum = sum + product;
-        sum1 = sum1 + credit[i];
+    temp_dump[j] = '\0';
+
+    strcpy(temp_file, filename);
+
+    for (i = len - 1; i >= 0; i--) {
+        if (temp_file[i] == '\\') {
+            break;
+        }
+        else {
+            temp_file[i] = '\0';
+        }
     }
 
-    double cgpa = (sum / sum1);
-    printf("%lf", cgpa);
+    strcat(temp_file, temp_dump);
+
+    strcpy(dump, temp_file);
+}
+
+int main(int argc, char *argv[])
+{
+    char s[500];
+    char filename[500], dumpfile[500];
+
+    strcpy(filename, argv[1]);
+
+    FILE *fp = fopen(filename, "r");
+
+    while (fgets(s, 500, fp) != NULL) {
+        printf("%s", s);
+
+        if (strstr(s, "$dumpfile") != NULL) {
+            strcpy(dumpfile, s);
+
+            break;
+        }
+    }
+
+    printf("\n\ndump = %s\n", dumpfile);
+
+    clean_dump(dumpfile, filename);
+
+    printf("\n\ndump = %s\n", dumpfile);
+
     return 0;
 }
