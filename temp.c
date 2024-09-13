05797c8c83c205e808c0dc790ce8df863779c082
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-09-13 18:23:19 +0600

    .

--- temp.c
+++ temp.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
-
-int len(int n)
-{
-    int l = 0;
-
-    while (n > 0) {
-        l++;
-        n /= 10;
-    }
-
-    return l;
-}
+#include <math.h>
 
 int main()
 {
-    int n, next, l;
+    double a = 0, b = 0, c = 0;
+    double d, x1, x2, re, im;
+    int i = 0;
 
-    scanf("%d", &n);
+    scanf("%lf %lf %lf", &a, &b, &c);
 
-    next = n + 1;
+    d = b * b - 4 * a * c;
 
-    l = len(next);
+    if (d < 0) {
+        re = (-b) / (2 * a);
+        im = sqrt(-d) / (2 * a);
+
+        if (im < 0) {
+            im *= -1;
+        }
+
+        printf("x1 = %g + %gi\nx2 = %g - %gi\n", re, im, re, im);
+    }
+    else if (d > 0) {
+        x1 = (-b + sqrt(d)) / (2 * a);
+        x2 = (-b - sqrt(d)) / (2 * a);
+
+        printf("x1 = %g\nx2 = %g\n", x1, x2);
+    }
+    else {
+        x1 = x2 = (-b) / (2 * a);
+
+        printf("x = %g\n", x1);
+    }
 
     return 0;
 }
