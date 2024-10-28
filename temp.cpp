78a47ae8baa9b7c29a7ac1bc72e5ae0863128b2c
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-10-28 09:52:54 +0600

    .

--- temp.cpp
+++ temp.cpp
@@ -4,19 +4,11 @@ using namespace std;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
 
-    int i, j;
-
-    for (i = 0; i < 17; i++) {
-        cout << (1 << i) << " = " << 1 << " ";
-
-        for (j = 0; j < i; j++) {
-            cout << "0 ";
-        }
-
-        cout << "\n";
+    for (int i = 0; i < 100000; i++) {
+        cout << i << "\n";
     }
 
     return 0;
