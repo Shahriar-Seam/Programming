91fee9f974640966c08c41507bb2c09d58f7fc32
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2024-11-23 19:09:34 +0600

    .

--- temp.cpp
+++ temp.cpp
@@ -1,25 +1,42 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    int t;
+    cin >> t;
 
-    int n;
-
-    cin >> n;
-
-    vector <int> v(n);
-
-    for (auto &it : v) {
-        cin >> it;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                int Max = arr[i];
+                for (int k = i; k <= j; k++)
+                {
+                    if (arr[k] > Max)
+                        Max = arr[k];
+                }
+                if (i == n - 1)
+                {
+                    cout << Max;
+                }
+                else
+                {
+                    cout << Max << " ";
+                }
+            }
+        }
+        cout << "\n";
     }
-
-    sort(v.begin(), v.end());
-
-    
-
-    return 0;
 }
