9145ac4ce68b5a7a3e7522c90003339738dcec47
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2025-05-05 10:11:23 +0600

    .

--- ATCoder/abc403_d.cpp
+++ ATCoder/abc403_d.cpp
@@ -7,8 +7,10 @@ int32_t main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n, d, count = 0;
+    int n, d, i, odd = 0, even = 0;
     map <int, int> mp;
+    vector <pair <int, int> > counter;
+    set <int> s;
 
     cin >> n >> d;
 
@@ -20,22 +22,33 @@ int32_t main()
         mp[it]++;
     }
 
-    set <int> s(v.begin(), v.end());
-
-    for (auto &it : s) {
-        if (mp[it] > mp[it - d]) {
-            count += mp[it - d];
-
-            mp.erase(it - d);
-        }
-        else {
-            count += mp[it];
-
-            mp.erase(it);
+    for (auto &it : mp) {
+        if (mp[it.first - d] > 0) {
+            s.insert(it.first);
+            s.insert(it.first - d);
         }
     }
 
-    cout << count << "\n";
+    v.assign(s.begin(), s.end());
+
+    for (i = 0; i < v.size(); i++) {
+        if (i % 2 == 0) {
+            even += mp[v[i]];
+        }
+        else {
+            odd += mp[v[i]];
+        }
+    }
+
+    for (auto &it : v) {
+        counter.push_back({mp[it], it});
+    }
+
+    sort(counter.begin(), counter.end());
+
+    
+
+    cout << min(even, odd) << "\n";
 
     return 0;
 }
