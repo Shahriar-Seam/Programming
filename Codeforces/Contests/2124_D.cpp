637dc6a99c3fa6eba15a28f5eb8ec959a37e8b95
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2025-07-07 21:04:14 +0600

    .

--- Codeforces/Contests/2124_D.cpp
+++ Codeforces/Contests/2124_D.cpp
@@ -1,154 +1,64 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <functional>
 #include <bits/stdc++.h>
 
 using namespace std;
-using namespace __gnu_pbds;
 
-typedef tree <pair <int, int>, null_type, less <pair <int, int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-
-int k_len(vector <int> &v, int k, vector <int> &p)
+void solve()
 {
-    int l = 0, r = v.size() - 1, count = 0;
-    vector <int> L, R;
+    int n, k, i, j, mx, l, r, d = 0;
+
+    cin >> n >> k;
+
+    vector <int> v(n), counter(n + 1, 0) , p;
+
+    p.reserve(n);
+
+    for (auto &it : v) {
+        cin >> it;
+
+        counter[it]++;
+    }
+    
+    for (i = 0, j = 0; i <= n; i++) {
+        j += counter[i];
+
+        if (j >= k) {
+            mx = i;
+            
+            break;
+        }
+    }
+
+    for (auto &it : v) {
+        if (it <= mx) {
+            p.emplace_back(it);
+        }
+    }
+
+    l = 0, r = p.size() - 1;
 
     while (l < r) {
-        if (v[l] == v[r]) {
-            count += 2;
-
-            L.push_back(v[l]);
-            R.push_back(v[r]);
-
+        if (p[l] == p[r]) {
             l++;
             r--;
         }
         else {
-            if (v[l] < v[r]) {
-                r--;
-            }
-            else {
+            if (p[l] == mx) {
                 l++;
+                d++;
             }
-        }
-    }
-
-    if (l == r) {
-        count++;
-    }
-
-    for (auto &it : L) {
-        p.push_back(it);
-    }
-
-    reverse(R.begin(), R.end());
-
-    for (auto &it : R) {
-        p.push_back(it);
-    }
-
-    return count;
-}
-
-bool is_palindrome(vector <int> &v)
-{
-    int i, n = v.size();
-
-    for (i = 0; i < n / 2; i++) {
-        if (v[i] != v[n - i - 1]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-void solve()
-{
-    int n, k, i, j, f = 0;
-    ordered_set os;
-
-    cin >> n >> k;
-
-    vector <int> v(n), p, temp;
-
-    for (i = 0; i < n; i++) {
-        cin >> v[i];
-        
-        os.insert({v[i], i});
-    }
-
-    int count = k_len(v, k, p);
-
-    // for (auto &it : v) {
-    //     cout << it << " ";
-    // }
-    // cout << "\n";
-
-    // for (auto &it : p) {
-    //     cout << it << " ";
-    // }
-    // cout << "\n";
-
-    i = j = 0;
-
-    while (i < p.size() && j < v.size()) {
-        if (p[i] == v[j]) {
-            i++;
-            j++;
-        }
-        else {
-            if (os.order_of_key({v[j], j}) >= k - 1) {
-                os.erase({v[j], j});
-
-                j++;
+            else if (p[r] == mx) {
+                r--;
+                d++;
             }
             else {
-                j++;
+                cout << "NO\n";
 
-                f++;
+                return;
             }
         }
     }
 
-    while (j < v.size()) {
-        if (os.order_of_key({v[j], j}) >= k - 1) {
-            os.erase({v[j], j});
-
-            j++;
-        }
-        else {
-            j++;
-
-            f++;
-        }
-    }
-
-    for (i = 0; i < n; i++) {
-        if (os.find({v[i], i}) != os.end()) {
-            temp.push_back(v[i]);
-        }
-    }
-
-    // for (auto &it : p) {
-    //     cout << it << " ";
-    // }
-    // cout << "\n";
-    
-    // for (auto &it : temp) {
-    //     cout << it << " ";
-    // }
-    // cout << "\n";
-
-    // cout << count << " " << k << " ";
-
-    // cout << f << "\n";
-
-    // for (auto &it : os) {
-    //     cout << it.first << " " << it.second << "\n";
-    // }
-
-    if (is_palindrome(temp) && f <= 2 && count >= k - 1) {
+    if (p.size() - d >= k - 1) {
         cout << "YES\n";
     }
     else {
