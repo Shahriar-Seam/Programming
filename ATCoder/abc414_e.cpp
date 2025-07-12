dca9ad04260f99cde211ba3b980041d671a3d225
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2025-07-12 19:59:18 +0600

    .

--- ATCoder/abc414_e.cpp
+++ ATCoder/abc414_e.cpp
@@ -8,25 +8,36 @@ using namespace std;
 const int mod = 998244353;
 const int mod_inv2 = 499122177;
 
-int32_t main()
+// sums n / 1 + n / 2 + n / 3 + ... + n / n
+// O(sqrt(n))
+int sum_n_by_k(int n, int mod = mod)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n, divs = 0, at = 1;
+    int sum = 0, at = 1;
     int add_amt, last_same;
-
-    cin >> n;
 
     while (at <= n) {
         add_amt = n / at;
 
         last_same = n / add_amt;
 
-        divs = (divs + ((add_amt * (last_same - at + 1)) % mod)) % mod;
+        sum = (sum + ((add_amt * (last_same - at + 1)) % mod)) % mod;
 
         at = last_same + 1;
     }
+
+    return sum;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, divs;
+
+    cin >> n;
+
+    divs = sum_n_by_k(n);
 
     n %= mod;
 
