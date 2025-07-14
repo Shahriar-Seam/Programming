3051a43d1ce99af9af6e92ec640bd61ef445d0aa
Shahriar Seam <i.a.shahriarseam@gmail.com>
2025-07-15 05:54:47 +0600

    .

--- temp.cpp
+++ temp.cpp
@@ -2,12 +2,33 @@
 
 using namespace std;
 
+void solve()
+{
+    int n, sum = 0, i;
+
+    cin >> n;
+
+    for (i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    cout << sum << "\n";
+}
+
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cout << (5 % 3 * 5 % 3) << "\n";
+    int32_t t, i;
+
+    cin >> t;
+
+    for (i = 1; i <= t; i++) {
+        // cout << "Case " << i << ": ";
+
+        solve();
+    }
 
     return 0;
 }
