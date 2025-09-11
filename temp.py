06c7f68033235151b96e6771760a3c8c3521e3d8
Ibnul Abrar Shahriar Seam <i.a.shahriarseam@gmail.com>
2025-09-11 16:30:45 +0600

    .

--- temp.py
+++ temp.py
@@ -1,1 +1,17 @@
-print("200000 " * 200000)
+def s(n):
+    ss = 0
+    
+    while n:
+        ss += n % 10
+        n //= 10
+        
+    return ss
+
+n = int(input())
+
+N = 0
+
+for i in range(1, n + 1):
+    N = N + int("9" * i)
+    
+    print(s(N))
