8d29128c454c47afd1d9660f72b673d309b7ebd3
Shahriar-Seam <i.a.shahriarseam@gmail.com>
2026-04-04 19:06:29 +0600

    .

--- CPP/Other CF Problems/1931_E.cpp
+++ CPP/Other CF Problems/1931_E.cpp
@@ -2,25 +2,68 @@
 
 using namespace std;
 
-int trailling_zeroes(int n)
+int trailing_zeroes(int n)
 {
     int count = 0;
 
     while (n % 10 == 0) {
         count++;
+
         n /= 10;
     }
 
     return count;
 }
 
+int digit_length(int n)
+{
+    int count = 0;
+
+    while (n > 0) {
+        count++;
+
+        n /= 10;
+    }
+
+    return count;
+}
+
+int non_zero_len(int n)
+{
+    return digit_length(n) - trailing_zeroes(n);
+}
+
 void solve()
 {
-    int n, m;
+    int n, m, len = 0, i;
 
     cin >> n >> m;
 
     vector <int> v(n);
+
+    for (auto &it : v) {
+        cin >> it;
+    }
+
+    sort(v.begin(), v.end(), [&] (int &a, int &b) {
+        return trailing_zeroes(a) > trailing_zeroes(b);
+    });
+
+    for (i = 0; i < n; i++) {
+        if (i & 1) {
+            len += digit_length(v[i]);
+        }
+        else {
+            len += non_zero_len(v[i]);
+        }
+    }
+
+    if (len > m) {
+        cout << "Sasha\n";
+    }
+    else {
+        cout << "Anna\n";
+    }
 }
 
 int32_t main()
